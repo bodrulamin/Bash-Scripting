74132c649aee928eb9605576e005d1816c9dcb7f
bodrulamin <bodrulaminiu@gmail.com>
2021-11-26 23:56:27 +0600

    updated

---
+++ readme.sh
@@ -0,0 +1,17 @@
+# Shell Scripting
+
+## Unix/Linux
+The Development of Unix started around 1969 at AT&T Bell Labs by `Ken Thompson and Dennis Ritchie.
+
+###What is Unix ?
+The Unix operating system is a set of programs that act as a link between the computer and the user.
+
+### Kernel
+The kernel is the heart of the operating system. it interacts with the hardware and most of thetasks like memory management, task sheduling and file management.
+
+
+### Shell 
+The Shell is the utility that processes your requests by interpreting the command. C Shell, Bourne Shell and Korn Shell are the most famous shells which are available with most of Unix variant.
+
+
+lsjkd
